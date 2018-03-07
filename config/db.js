3060c6ede81467362841ae1f1f011af0be0522ce
Herbie <843834089@qq.com>
2018-03-07 23:24:19 +0800

    连接数据库

---
+++ config/db.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+var pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "123321",
+  database: "octpark"
+});
+
+function query(sql,callback){
+    pool.getConnection(function(err,connection){
+        connection.query(sql, function (err,rows) {
+            callback(err,rows);
+            connection.release();
+        });
+    });
+}
+
+exports.query = query;
+
+
+// var mysql = require("mysql");
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123321",
+//   database:"octpark"
+// });
+
+// connection.connect();
+
+//   connection.query("SELECT 2 + 2 AS solution", function(err, rows, fields) {
+//     if (err) throw err;
+//     console.log("The solution is: ", rows[0].solution);
+//   });
+
+//   connection.query('SELECT id FROM `opark`',function(error, results, fields){
+//     if(error) throw error;
+//     console.log(results);
+//     // console.log(fields);
+// })
+
+//   connection.end();
+
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,39 @@
 var express = require("express");
 var router = express.Router();
+var db = require("../config/db");
 
-// header("Access-Control-Allow-Origin :".$origin); //允许的域名（ * 所有域） 
-// header("Access-Control-Allow-Methods : POST"); //允许的方法 
+//header三大件
+
+// header("Access-Control-Allow-Origin :".$origin); //允许的域名（ * 所有域）
+// header("Access-Control-Allow-Methods : POST"); //允许的方法
 // header("Access-Control-Allow-Headers : x-requested-with , content-type"); //服务器支持的头信息
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   // res.send("respond with a resource");
-  var a = {
-    c:2
-  }
-  a.b = 1;
 
-  res.send(a);
+  // sql = "SELECT 2 + 2 AS solution";
+  // sql = "SELECT id FROM `opark`";
+  // sql = "SELECT * FROM `opark` WHERE area=100";
+  sql = "SELECT `name`, address FROM `opark`;";
+  db.query(sql, function(err, rows) {
+    if (err) {
+      throw err;
+    } else {
+      // console.log("The solution is: ", rows[0].solution);
+      console.log(rows)
+      res.send(rows);
+    }
+  });
+
+  var a = {
+    c: 2
+  };
+  a.b = 1;
+  a.q = req.query.id; //获取query参数
+  // res.send(a);
 });
 
 module.exports = router;
+
+
